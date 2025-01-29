066e489849747489c62a89233ac93ec2594fa227
Miko 3 <sunsetsalisz@gmail.com>
2025-01-29 23:08:47 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+
+using std::cout;
+using std::cin;
+
+
+int main() {
+
+    char a;
+    double x;
+    double y;
+    double wynik;
+
+    cout << "Kalkulator!\n";
+    cout << "Wybierz działanie (| + | - | * | / |): ";
+    cin >> a;
+    cout << "\nPodaj pierwszą liczbę: ";
+    cin >> x;
+    cout << "\nPodaj drugą liczbę: ";
+    cin >> y;
+
+    switch (a)
+    {
+    case '+':
+        wynik = x + y;
+        cout << "Wynik dodawania: " << wynik;
+        break;
+    case '-':
+        wynik = x - y;
+        cout << "Wynik odejmowania: " << wynik;
+        break;
+    case '*':
+        wynik = x * y;
+        cout << "Wynik mnożenia: " << wynik;
+        break;
+    case '/':
+        wynik = x / y;
+        cout << "Wynik dzielenia: " << wynik;
+        break;
+    
+    default:
+        cout << "Wybierz poprawną operację!";
+        break;
+    }
+
+
+
+
+
+}
